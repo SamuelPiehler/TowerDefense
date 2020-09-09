712cffc3022cd081030c4d4a32c2b2bb3af0e222
Samuel <samuel.piehler@gmail.com>
2020-09-09 16:06:20 +0200

    anzeige wv gegner vom sniper gehittet wurden

--- js/tower.js
+++ js/tower.js
@@ -425,6 +425,8 @@ function Turm(posx, posy, typ, id, spezialisierung) {
               }
               break;
           }
+          //kommt weg!!
+          var anzalHits = 0;
           for (var i = gegner.length - 1; i >= 0 ; i--) {
             var item = gegner[i];
             if (item != undefined) {
@@ -432,21 +434,25 @@ function Turm(posx, posy, typ, id, spezialisierung) {
                 case 0:   //wenn der sniper exact nach oben zeigt
                   if (this.posy > item.posy && Math.abs(this.posx-item.posx) <= size/2) {
                     item.damage(this.schaden*(1+this.buffStaerken[0]/100), this.effekt.slice(), uebergabeEffektStaerke.slice(), uebergabeEffektTime.slice(), this.id);
+                    anzalHits++;
                   }
                   break;
                 case 90:   //wenn der sniper exact nach links zeigt
                   if (this.posx > item.posx && Math.abs(this.posy-item.posy) <= size/2) {
                     item.damage(this.schaden*(1+this.buffStaerken[0]/100), this.effekt.slice(), uebergabeEffektStaerke.slice(), uebergabeEffektTime.slice(), this.id);
+                    anzalHits++;
                   }
                   break;
                 case 180:   //wenn der sniper nach unten zeigt
                   if (this.posy < item.posy && Math.abs(this.posx-item.posx) <= size/2) {
                     item.damage(this.schaden*(1+this.buffStaerken[0]/100), this.effekt.slice(), uebergabeEffektStaerke.slice(), uebergabeEffektTime.slice(), this.id);
+                    anzalHits++;
                   }
                   break;
                 case 270:  //wenn der sniper nach rechts zeigt
                   if (this.posx < item.posx && Math.abs(this.posy-item.posy) <= size/2) {
                     item.damage(this.schaden*(1+this.buffStaerken[0]/100), this.effekt.slice(), uebergabeEffektStaerke.slice(), uebergabeEffektTime.slice(), this.id);
+                    anzalHits++;
                   }
                   break;
                 default:   //f(x)=a(x-this.posx)+this.posy funktion der geraden auf der der sniperschuss fliegt   //g(x)=1/a(x-item.posx)+item.posy funktion der gerade gegner und dem nähesten punkt der snipergerade
@@ -456,12 +462,14 @@ function Turm(posx, posy, typ, id, spezialisierung) {
                   if (entfernung <= 35) {
                     if ((this.richtung < 180 && this.posx > item.posx) || (this.richtung > 180 && this.posx < item.posx)) {
                       item.damage(this.schaden*(1+this.buffStaerken[0]/100), [], [], [], this.id);
+                      anzalHits++;
                     }
                   }
                   break;
               }
             }
           }
+          console.log(anzalHits);
         }
         else if (this.typ == 5 && this.upgradeStufe == maxUpgrade && towertypen[this.typ][12] && target2 !== -1) {    //wenn antiBoss stufe 5
           bullet(this.posx, this.posy, gegner[target2].posx, gegner[target2].posy, 100/gameSpeed);   //add bullet
