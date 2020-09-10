0002a9ac344011ee1a60f806f7f546aaefa796a2
Samuel <samuel.piehler@gmail.com>
2020-09-10 14:16:42 +0200

    slow tower bekommt kein atackspeed mehr fix anti boss fähigkeit

--- bodyScript.js
+++ bodyScript.js
@@ -728,13 +728,18 @@ function showUpgrade(object, id) {
     upgradeFenster.innerHTML += "Reichweite: " + round(tuerme[id].reichweite*(1+tuerme[id].buffStaerken[3]/100), 3) + " <br>";
     }
     else {
+      if (typ == 2) {
+        upgradeFenster.innerHTML += "Nachladezeit: " + round(tuerme[id].angriffsZeit/(1+tuerme[id].buffStaerken[1]/100), 3) + " sec <br>";
+      }
+      else {
         upgradeFenster.innerHTML += "Nachladezeit: " + round(tuerme[id].angriffsZeit/(1+tuerme[id].buffStaerken[1]/100), 3) + " <font color=ff0000>-" + round(towertypen[typ][5]/10/(1+tuerme[id].buffStaerken[0]/100), 3) + "</font> sec <br>";
-        if (typ == 9) {
-          upgradeFenster.innerHTML += "Reichweite: " + round(tuerme[id].reichweite*(1+tuerme[id].buffStaerken[3]/100), 2) + " <font color=ff0000>+" + round(towertypen[typ][9][tuerme[id].effekt[0]-7]*0.1*(1+tuerme[id].buffStaerken[2]/100), 2) + " </font><br>";
-        }
-        else {
-          upgradeFenster.innerHTML += "Reichweite: " + round(tuerme[id].reichweite*(1+tuerme[id].buffStaerken[3]/100), 2) + " <font color=ff0000>+" + round(towertypen[typ][4]/10*(1+tuerme[id].buffStaerken[3]/100), 2) + " </font><br>";
-        }
+      }
+      if (typ == 9) {
+        upgradeFenster.innerHTML += "Reichweite: " + round(tuerme[id].reichweite*(1+tuerme[id].buffStaerken[3]/100), 2) + " <font color=ff0000>+" + round(towertypen[typ][9][tuerme[id].effekt[0]-7]*0.1*(1+tuerme[id].buffStaerken[2]/100), 2) + " </font><br>";
+      }
+      else {
+        upgradeFenster.innerHTML += "Reichweite: " + round(tuerme[id].reichweite*(1+tuerme[id].buffStaerken[3]/100), 2) + " <font color=ff0000>+" + round(towertypen[typ][4]/10*(1+tuerme[id].buffStaerken[3]/100), 2) + " </font><br>";
+      }
     }
     if (tuerme[id].drehGeschw != 0) {
       upgradeFenster.innerHTML += "Drehgeschwindigkeit: " + round(tuerme[id].drehGeschw*100*(1+tuerme[id].buffStaerken[3]/100), 2) + " <font color=ff0000>+" + round(towertypen[typ][3]*10*(1+tuerme[id].buffStaerken[3]/100), 2) + " </font> Grad/sec <br>";
--- js/skills.js
+++ js/skills.js
@@ -13,3 +13,24 @@ const skills = [];
 //für alle Türme
 //hat gleichen skills wie je turm nur unlocked wenn x Punkte in je turm stat verteilt
 //Max Upgradestufe von 3-5 erhöhen
+
+
+//allgemeine Upgrades
+//+Geld pro kill
+//Steine zerstörbar machen für 1000 Geld
+//Steine zerstören billiger machen
+
+//Upgrades für Spezifische tower
+//Randomtower bekommt chance turm mit upgrades zu spawnen
+//Tesla bekommt extra Targets auf dem skill
+//Sniper ST 5 Breiterer schuss
+//St5 Mehr Läufe
+//Slow St 5 mehr max Stats
+//Poisen Ignoriert teil von immunität
+//AoE Höhere chance auf specialAngriff
+//Zweiten Lauf
+//Rocket längere Stunzeit+Efecktrange
+//SingleGift bekommt kleinen Aoe beim gegner
+//Lava Setzt feld um sich in Brand
+//support kann 2te fähigkeit wählen
+//Kettenblitz hat chance auf 2 Gegner überspringen
