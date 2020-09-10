ab97da49a4cfffb98a5dfac601b70891a4d7cd15
Samuel <samuel.piehler@gmail.com>
2020-09-10 12:55:59 +0200

    skillListe

---
+++ js/skills.js
@@ -0,0 +1,15 @@
+//0 = Name, 1 = Beschreibung, 2 = TowerAffeckted, 3 = StattAffeckted, 4 = Stattchange, 5 = SkillRequired, 6 = SkillLvl Required, 7 = MaxStufe, 8 = MomentaneStufe
+const skills = [];
+//für jeden Turm
+
+//+schaden/efecktschaden(+% bis auf eis + total) 2 skills
+//+angriffsGeschwindikeit//nichtSupport
+//+reichweite
+//+drehGeschw
+//+critChance
+//+critDamage
+//+st5Unlock benötigt allgemeine upgradestufe auf 5
+
+//für alle Türme
+//hat gleichen skills wie je turm nur unlocked wenn x Punkte in je turm stat verteilt
+//Max Upgradestufe von 3-5 erhöhen
