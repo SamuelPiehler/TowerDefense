779e9317f613b58201985f37c5a89a2f0cc6d4cd
Samuel <samuel.piehler@gmail.com>
2020-09-28 10:07:23 +0200

    geldgain generft

--- js/tower_Gegner_Stats.js
+++ js/tower_Gegner_Stats.js
@@ -17,22 +17,23 @@ var orginalTowertypen = [
 
 var towertypen = copyObj(orginalTowertypen);
 
-var gegnertypen = [];
-//0 = Bild, 1 = Leben, 2 = Geschwindigkeit, 3 = Imunität/effekt, 4 = Imunität%/effecktstärke, 5 = GeldBeiKill, 6= Spielerschaden
-gegnertypen.push(['Bilder/Gegner/gegner00Basic.png', 120, 0.8, [], [], 1, 1]);   //0 Basic
-gegnertypen.push(['Bilder/Gegner/gegner01BonusHp.png', 240, 1, [], [], 2, 2]);   //1 Basic mit etwas mehr Leben
-gegnertypen.push(['Bilder/Gegner/gegner02LangsamTank.png', 450, 0.6, [0], [50], 3, 3]);  //2 Langsamer Gegner mit viel Leben slowimmunität
-gegnertypen.push(['Bilder/Gegner/gegner03Schnell.png', 200, 1.6, [], [], 4, 4]);   //3 Schneller Gegner
-gegnertypen.push(['Bilder/Gegner/gegner04Immunitaeten.png', 400, 0.9, [2,3,5], [75,100,20], 5, 5]);   //4 etwas Langsamer Gegner mit vielen Immunitäten
-gegnertypen.push(['Bilder/Gegner/gegner05GiftImmun.png', 350, 0.9, [3], [75], 5, 5]);  //5 Gift immuner Gegner
-gegnertypen.push(['Bilder/Gegner/gegner06Boss1.png', 1000, 0.5, [0,6], [65,25], 20, 10]);   //6 Boss Gegner langsam mit slow und normaldmg immunität
-gegnertypen.push(['Bilder/Gegner/gegner07Agro.png', 800,  1 ,[9] , [1], 5 , 3]);  //7 zieht die Tower auf sich
-gegnertypen.push(['Bilder/Gegner/gegner08Heiler.png', 650, 1 ,[10], [[100, 140, 1]], 10 , 4]);  //8 soll die Gegner heilen 1% jede sec(100 spielticks) mit 140 reichweite
-gegnertypen.push(['Bilder/Gegner/gegner09SpawnTimer.png', 550, 0.6, [8], [[250, 0]], 15, 6]); //9 spawnt Gegner 1 alle 2.5 sec (=250)
-gegnertypen.push(['Bilder/Gegner/gegner10DethSplit.png', 400, 0.9 ,[7], [3] , 10, 6]); //10 nach dem Tod spawnt er 3 mal Gegner 11
-gegnertypen.push(['Bilder/Gegner/gegner11Dethsplit2.png', 300, 1.4 ,[], [] , 5, 1]); // 11 wird von anderen gegnern gespawned
-gegnertypen.push(['Bilder/Gegner/gegner12SpeedBuff.png', 375, 1.2 ,[11], [[5, 140]], 10 , 6]); //12 Bufft speed von anderen Gegner um 5% 140 reichweite
-gegnertypen.push(['Bilder/Gegner/gegner13Shield.png', 650 , 1.6, [12], [[40, 140]], 10, 6]); //13 Bufft Gegner mit Schild 50% von gegnerleben und der Effektschaden wird anulliert solange das schild aktiv ist
-gegnertypen.push(['Bilder/Gegner/gegner14TowerSlow.png', 750, 0.7, [13, 14], [[5, 350], [500, 140, 100]], 15 , 5]); //14 slowed die Türme in 350 reichweite um 5% und stunned alle 5 sec einen turm in 140 reichweite für 1 sec
-gegnertypen.push(['Bilder/Gegner/gegner15Boss2.png', 2000, 0.8, [], [], 30 , 15]); //15 Boss2
-gegnertypen.push(['Bilder/Gegner/gegner16Boss3.png', 2500, 0.7, [8], [[1500, 8]], 35, 15]); // 16 Boss3 spawnt alle 15 sec gegner 8 (heiler)
+var gegnertypen = [
+  //0 = Bild, 1 = Leben, 2 = Geschwindigkeit, 3 = Imunität/effekt, 4 = Imunität%/effecktstärke, 5 = GeldBeiKill, 6= Spielerschaden
+  ['Bilder/Gegner/gegner00Basic.png', 120, 0.8, [], [], 1, 1],   //0 Basic
+  ['Bilder/Gegner/gegner01BonusHp.png', 240, 1, [], [], 2, 2],   //1 Basic mit etwas mehr Leben
+  ['Bilder/Gegner/gegner02LangsamTank.png', 450, 0.6, [0], [50], 3, 3],  //2 Langsamer Gegner mit viel Leben slowimmunität
+  ['Bilder/Gegner/gegner03Schnell.png', 200, 1.6, [], [], 4, 3],   //3 Schneller Gegner
+  ['Bilder/Gegner/gegner04Immunitaeten.png', 400, 0.9, [2,3,5], [75,100,20], 5, 4],   //4 etwas Langsamer Gegner mit vielen Immunitäten
+  ['Bilder/Gegner/gegner05GiftImmun.png', 350, 0.9, [3], [75], 5, 3],  //5 Gift immuner Gegner
+  ['Bilder/Gegner/gegner06Boss1.png', 1000, 0.5, [0,6], [65,25], 20, 10],   //6 Boss Gegner langsam mit slow und normaldmg immunität
+  ['Bilder/Gegner/gegner07Agro.png', 800,  1 ,[9] , [1], 5 , 3],  //7 zieht die Tower auf sich
+  ['Bilder/Gegner/gegner08Heiler.png', 650, 1 ,[10], [[100, 140, 1]], 10 , 3],  //8 soll die Gegner heilen 1% jede sec(100 spielticks) mit 140 reichweite
+  ['Bilder/Gegner/gegner09SpawnTimer.png', 550, 0.6, [8], [[250, 0]], 15, 4], //9 spawnt Gegner 1 alle 2.5 sec (=250)
+  ['Bilder/Gegner/gegner10DethSplit.png', 400, 0.9 ,[7], [3] , 10, 2], //10 nach dem Tod spawnt er 3 mal Gegner 11
+  ['Bilder/Gegner/gegner11Dethsplit2.png', 300, 1.4 ,[], [] , 5, 1], // 11 wird von anderen gegnern gespawned
+  ['Bilder/Gegner/gegner12SpeedBuff.png', 375, 1.2 ,[11], [[5, 140]], 10 , 3], //12 Bufft speed von anderen Gegner um 5% 140 reichweite
+  ['Bilder/Gegner/gegner13Shield.png', 650 , 1.6, [12], [[40, 140]], 10, 4], //13 Bufft Gegner mit Schild 50% von gegnerleben und der Effektschaden wird anulliert solange das schild aktiv ist
+  ['Bilder/Gegner/gegner14TowerSlow.png', 750, 0.7, [13, 14], [[5, 350], [500, 140, 100]], 15 , 4], //14 slowed die Türme in 350 reichweite um 5% und stunned alle 5 sec einen turm in 140 reichweite für 1 sec
+  ['Bilder/Gegner/gegner15Boss2.png', 2000, 0.8, [], [], 30 , 12], //15 Boss2
+  ['Bilder/Gegner/gegner16Boss3.png', 2500, 0.7, [8], [[1500, 8]], 35, 15], // 16 Boss3 spawnt alle 15 sec gegner 8 (heiler)
+];
