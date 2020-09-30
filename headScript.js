f5bcd6cfb695b7a0926e991de2d13a7c4ce82933
Samuel <samuel.piehler@gmail.com>
2020-09-30 14:52:09 +0200

    fix dennis sein schitt

--- headScript.js
+++ headScript.js
@@ -16,7 +16,7 @@ var loading = 0;
 var start = [[], []];  //array in dem x und y koordinate des startpunktes abgespeichert werden
 var portal1 = [[], [], []]; //zu abspeichern der portalPositionen
 var spawnPointNumber = 0;
-if (multiStartTyp == undefined) {
+if (multiStartTyp == "undefined") {
   var multiStartTyp = 0;   //0=Nacheinander, 1=pro welle, 2=zufällig, 3= überall gleichzeitig
 }
 var teilWellenNummer = 0;   // giebt an welche gegnerwelle aus dem array als nächses gespwned wird
